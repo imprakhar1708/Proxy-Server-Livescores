6039c0102ad58253290bcf005195466ddc5f18c6
Prakhar Shrivastava <114819970+imprakhar1708@users.noreply.github.com>
2023-01-05 10:54:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fetch = (...args) =>
 var requestOptions = {
 	method: "GET",
 	headers: {
-		"x-rapidapi-key": "8ea474ed8d5e143a9b74941ea336b09e",
+		"x-rapidapi-key": "2eeafea8379edba2af77907f4010b282",
 		"x-rapidapi-host": "v3.football.api-sports.io",
 	},
 	redirect: "follow",
@@ -16,45 +16,26 @@ var requestOptions = {
 app.use(
 	cors({
 		Credentials: true,
-		origin: ["3.75.158.163", "3.125.183.140", "35.157.117.28"],
+		origin: ["http://localhost:3000"],
 	})
 );
 app.listen(port);
 
-/****************************STANDINGS***************************/
-let dataSDPL;
-let dataSDBL;
-let dataSDLL;
-const fetchDataSDPL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/standings?league=39&season=2022`,
-		requestOptions
-	);
-	dataSDPL = await r.json();
+
+/***********************LIVESCORE*************************/
+let dataL;
+const fetchDataL = async () => {
+	const apiUrl1 = `https://v3.football.api-sports.io/fixtures?league=39&season=2022&last=5`;
+	const apiUrl2 = `https://v3.football.api-sports.io/fixtures?league=2&season=2022&last=5`;
+	const res1 = await fetch(apiUrl1, requestOptions);
+	const res2 = await fetch(apiUrl2, requestOptions);
+	const r1 = await res1.json();
+	const r2 = await res2.json();
+	dataL = [r1, r2];
 };
-const fetchDataSDBL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/standings?league=78&season=2022`,
-		requestOptions
-	);
-	dataSDBL = await r.json();
-};
-const fetchDataSDLL = async () => {
-	const r = await fetch(
-		`https://v3.football.api-sports.io/standings?league=140&season=2022`,
-		requestOptions
-	);
-	dataSDLL = await r.json();
-};
-fetchDataSDPL();
-fetchDataSDLL();
-fetchDataSDBL();
-app.get("standings/39", async (req, res) => {
-	res.json(dataSDPL);
+fetchDataL();
+
+app.get("/livescores", async (req, res) => {
+	res.json(dataL);
 });
-app.get("standings/140", async (req, res) => {
-	res.json(dataSDLL);
-});
-app.get("standings/78", async (req, res) => {
-	res.json(dataSDBL);
-});
+
